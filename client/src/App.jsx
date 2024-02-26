313b3e12b277358837ab91ef6261e40c9c87a64f
Neelesh B Shetty <neeleshshetty1236@gmail.com>
2024-02-26 16:23:01 +0530

    Protected routes added to Profile Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter,Routes,Route } from "react-router-dom"
 import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
@@ -8,8 +9,8 @@ import Signup from "./pages/Signup";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Header />
+		<BrowserRouter>
+			<Header />
 			<Routes>
 				<Route
 					path="/"
@@ -35,10 +36,12 @@ const App = () => {
 					path="/sign-up"
 					element={<Signup />}
 				/>
-				<Route
-					path="/profile"
-					element={<Profile />}
-				/>
+				<Route element={<PrivateRoute />}>
+					<Route
+						path="/profile"
+						element={<Profile />}
+					/>
+				</Route>
 			</Routes>
 		</BrowserRouter>
 	);
